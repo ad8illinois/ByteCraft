e2da7bac416554521824659ebbf190d788a9693c
Annamika Dua <annamika@annamikas-mbp.lan>
2023-12-07 23:19:53 -0500

    knn implementation, tested with dummy data

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import numpy as np
 from inverted_index import InvertedIndex
 from prob_dist import vector_to_prob_dist
 from util import term_vec_to_file
+from ml_model_definitions import knn_classification
 
 @click.group()
 def cli():
@@ -28,43 +29,41 @@ def learn(input_dir, output_dir):
 
     Estimates a ML Unigram LM for each topic given, and writes the LMs to an output directory.
     """
-    topic_documents = {
-        'animals': [
-            './testdata/wikipedia/bird.txt',
-            './testdata/wikipedia/cat.txt',
-            './testdata/wikipedia/dog.txt',
-            './testdata/wikipedia/fish.txt',
-        ],
-        'places': [
-            './testdata/wikipedia/champaign.txt',
-            './testdata/wikipedia/chicago.txt',
-            './testdata/wikipedia/uiuc.txt',
-        ],
-        'corpus': {
-            './testdata/wikipedia/bird.txt',
-            './testdata/wikipedia/cat.txt',
-            './testdata/wikipedia/dog.txt',
-            './testdata/wikipedia/fish.txt',
-            './testdata/wikipedia/champaign.txt',
-            './testdata/wikipedia/chicago.txt',
-            './testdata/wikipedia/uiuc.txt',
-        }
-    }
-
     # topic_documents = {
-    #     'corpus': [
-    #         './testdata/dummy/1.txt',
-    #         './testdata/dummy/2.txt',
-    #         './testdata/dummy/3.txt',
+    #     'animals': [
+    #         './testdata/wikipedia/bird.txt',
+    #         './testdata/wikipedia/cat.txt',
+    #         './testdata/wikipedia/dog.txt',
+    #         './testdata/wikipedia/fish.txt',
     #     ],
-    #     'happy': [
-    #         './testdata/dummy/1.txt',
-    #         './testdata/dummy/3.txt',
+    #     'places': [
+    #         './testdata/wikipedia/champaign.txt',
+    #         './testdata/wikipedia/chicago.txt',
+    #         './testdata/wikipedia/uiuc.txt',
     #     ],
-    #     'sad': [
-    #         './testdata/dummy/2.txt',
-    #     ],
+    #     'corpus': {
+    #         './testdata/wikipedia/bird.txt',
+    #         './testdata/wikipedia/cat.txt',
+    #         './testdata/wikipedia/dog.txt',
+    #         './testdata/wikipedia/fish.txt',
+    #         './testdata/wikipedia/champaign.txt',
+    #         './testdata/wikipedia/chicago.txt',
+    #         './testdata/wikipedia/uiuc.txt',
+    #     }
     # }
+
+    topic_documents = {
+        'happy': [
+            './testdata/dummy/1.txt',
+            './testdata/dummy/3.txt',
+            './testdata/dummy/4.txt',
+            './testdata/dummy/6.txt',
+        ],
+        'sad': [
+            './testdata/dummy/2.txt',
+            './testdata/dummy/5.txt',
+        ],
+    }
 
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
@@ -88,9 +87,11 @@ def learn(input_dir, output_dir):
     # Create a tf-vector, prob-dist for each file
     # NOTE: you could skip this step, it's not needed to calculate the prob-dist for each category
     terms = inverted_index.get_terms()
+    X_knn = []
     for filepath in filepaths:
         print(f'Creating tf-vector, prob-dist for file: {filepath}')
         tf_vector = inverted_index.get_tf_vector(filepath, pseudo_counts=1)
+        X_knn.append(tf_vector)
         prob_dist = vector_to_prob_dist(tf_vector)
 
         output_filename = os.path.basename(filepath)
@@ -114,6 +115,31 @@ def learn(input_dir, output_dir):
         output_filepath = os.path.join(output_dir, 'categories', f'{topic}.txt')
         term_vec_to_file(terms, topic_tf_vector, output_filepath.replace('.txt', '_tf.txt'))
         term_vec_to_file(terms, topic_prob_dist, output_filepath.replace('.txt', '_prob_dist.txt'))
+
+    # KNN Classification
+    print('Performing KNN classification with k=2...')
+    X = X_knn
+    # print(topic_documents)
+    y = []
+    for key, value in topic_documents.items():
+        for i in value:
+            y.append(list(topic_documents).index(key))
+    # print(y)
+    print('Classification Results:')
+    X_test, knn = knn_classification(2, X, y)
+    # print("x test", X_test, type(X_test))
+    files = []
+    for filepath in filepaths:
+        tf_vector = inverted_index.get_tf_vector(filepath, pseudo_counts=1)
+        # print("current tf vector", type(tf_vector))
+        if np.any(np.all(tf_vector == X_test, axis=1)):
+            files.append(filepath)
+    # print("Files", files)
+    print("Prediction for files", files, ":")
+    pred = []
+    for i in knn:
+        pred.append(list(topic_documents)[i])
+    print(pred)
    
 
 @click.command()
